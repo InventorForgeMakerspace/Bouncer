28729a588af6bebda3b29b5144f44fff2b60e2ae
scratchhax <scratchhax@users.noreply.github.com>
2016-08-27 12:05:12 -0500

    Create doorlock.py

---
+++ doorlock.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+#
+#Lock the door. 
+#
+import time
+import pifacedigitalio
+
+pfd = pifacedigitalio.PiFaceDigital(init_board=False)
+from slacker import Slacker
+log = open("/tmp/doorlog.txt", "a")
+
+slack = Slacker('xoxb-*****************************')
+
+slack.chat.post_message('#door', 'The forge door is locked at ' +time.strftime("%H:%M:%S") +' CST' +'\n')
+log.write(time.strftime("%H:%M:%S") +" The door has been locked" +'\n')
+pfd.relays[1].value = 0
+log.close()

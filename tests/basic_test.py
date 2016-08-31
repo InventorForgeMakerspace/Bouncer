12a832fe8bb9481b4944d2f55a0d4eb8852da62a
Robert Richter <robert.c.richter@gmail.com>
2016-08-30 19:55:54 -0500

    Add imports so we at least test that

--- tests/basic_test.py
+++ tests/basic_test.py
@@ -1,4 +1,12 @@
 import unittest
+import os
+import sys
+sys.path.insert(0, os.path.abspath('..'))
+
+from bouncer.door import doorcheck
+from bouncer.door import doorcounter
+from bouncer.door import doorlock
+from bouncer.door import doorunlock
 
 class TestBasic(unittest.TestCase):
     def test_nothing(self):
